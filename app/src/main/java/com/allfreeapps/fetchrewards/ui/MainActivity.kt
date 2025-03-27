86e5409b1d14052fe0156eb353e69cf8a0eda214
serdar selcuk <serdarselcu61@gmail.com>
2025-03-26 23:46:59 -0500

    composables moved in to a separated package

--- app/src/main/java/com/allfreeapps/fetchrewards/ui/MainActivity.kt
+++ app/src/main/java/com/allfreeapps/fetchrewards/ui/MainActivity.kt
@@ -5,112 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.allfreeapps.balanser_tool.ui.theme.My_Theme
-import com.allfreeapps.fetchrewards.model.Reward
+import com.allfreeapps.fetchrewards.ui.composables.MainScreen
 import com.allfreeapps.fetchrewards.viewModel.FetchRewardsViewModel
 
 class MainActivity : ComponentActivity() {
     private val viewModel: FetchRewardsViewModel by viewModels()
+    private lateinit var mainScreen: MainScreen
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        mainScreen = MainScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             My_Theme {
-                Scaffold (modifier = Modifier.fillMaxSize()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    FetchRewardsApp(innerPadding)
+                    mainScreen.FetchRewardsApp(
+                        innerPadding,
+                        viewModel
+                    )
                 }
             }
-        }
-    }
-
-    fun callRewardsService(){
-        viewModel.getRewards()
-    }
-
-    @Composable
-    fun FetchRewardsApp(innerPadding: PaddingValues){
-        val rewardsResponse: List<Reward>? = viewModel.rewardsResponse.observeAsState().value
-
-
-        Column(
-            modifier=Modifier
-                .fillMaxSize()
-                .padding(top = innerPadding.calculateTopPadding()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
-
-        ) {
-            Box {
-                ElevatedButton(
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .wrapContentHeight()
-                        .padding(16.dp),
-                    onClick = { callRewardsService() }
-                ) {
-                    Text("Click to see the list")
-                }
-            }
-            Box(modifier = Modifier.fillMaxSize()){
-                if (!rewardsResponse.isNullOrEmpty()) {
-                    RewardList(rewardsResponse)
-                } else {
-                    Text(text = "No text available",
-                        modifier = Modifier.align(Alignment.Center))
-                }
-            }
-        }
-
-
-    }
-
-    @Composable
-    fun RewardList(response: List<Reward>) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
-            items(response.size) { index ->
-                MessageRow(response[index])
-                HorizontalDivider(thickness = 2.dp)
-            }
-        }
-    }
-
-    private @Composable
-    fun MessageRow(reward: Reward) {
-        Row {
-            Text(text = "Name: ${reward.name}, Id: ${reward.id}, ListId: ${reward.listId}")
-        }
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun PreviewFetchRewardsApp(){
-        My_Theme {
-            FetchRewardsApp(innerPadding = PaddingValues(15.dp))
         }
     }
 }
